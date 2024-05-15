0a3075b65b0d34efe5e54e98461aedb15e717475
Jackelyn FDZ <2130173@upv.edu.mx>
2024-05-15 10:06:23 -0600

    Primer commit
    Subir los archivos desde Github Desktop

---
+++ resources/views/alumnos.blade.php
@@ -0,0 +1,11 @@
+<!-- invoca el archivo patron de diseño (layout) -->
+@extends('layouts.app')
+
+<!-- crear el contenido del titulo para que sea insertado en la estructura app -->
+@section('titulo')
+    Alumnos
+@endsection
+
+@section('contenido')
+    Contenido de la página alumnos
+@endsection
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,11 @@
+<!-- invoca el archivo patron de diseño (layout) -->
+@extends('layouts.app')
+
+<!-- crear el contenido del titulo para que sea insertado en la estructura app -->
+@section('titulo')
+    Pagina principal
+@endsection
+
+@section('contenido')
+    Bienvenidos a todos, esta es la seccion de contenido
+@endsection
